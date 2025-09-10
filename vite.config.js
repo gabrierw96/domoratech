3659c76e86589fc4a019e33ee2d877950a6ace89
gabrierw96 <gabrierw@hotmail.com>
2025-09-10 17:46:07 -0400

    Fix Vercel blank page (vite.config + index.html)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: 'dist' }
+  base: '/', // 👈 importante para que funcione en Vercel
 })
